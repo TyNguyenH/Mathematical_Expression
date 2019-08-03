e6919b8da1d86cd110ab914bd45bb8557bd0de21
TyNguyenH <NguyenHuuTy189@gmail.com>
2019-08-03 16:23:12 +0700

    Add small changes

--- Test.java
+++ Test.java
@@ -1,9 +1,13 @@
 import MathematicalExpression.*;
 
 class Test {
-    // public static Operand generateRandomOperand() {
+    // generate random operand, (-100 < coefficient < 100 && 0 <= exponent < 10)
+    public static Operand generateRandomOperand() {
+        double coefficient = Math.random() * 100 + -100;
+        int exponent = (int) Math.round((Math.random() * 10) + 0);
 
-    // }
+        return new Operand(coefficient, exponent);
+    }
     
     public static void main(String[] args) {
         System.out.print(".--------------------------------------------------------." + "\n"
@@ -15,16 +19,16 @@ class Test {
         
         Operand arrayOfOperands[] = new Operand[10];
         
-        arrayOfOperands[0] = new Operand(1.1, 2);
-        arrayOfOperands[1] = new Operand(3, 1);
-        arrayOfOperands[2] = new Operand(5.6, 1);
-        arrayOfOperands[3] = new Operand(6, 3);
-        arrayOfOperands[4] = new Operand(-7.9, 3);
-        arrayOfOperands[5] = new Operand(2, 2);
-        arrayOfOperands[6] = new Operand(4.2, 4);
-        arrayOfOperands[7] = new Operand(0, 1);
-        arrayOfOperands[8] = new Operand(-9, 5);
-        arrayOfOperands[9] = new Operand(0.9, 5);
+        arrayOfOperands[0] = Operand.parseOperand("1.1x^2");
+        arrayOfOperands[1] = Operand.parseOperand("3x");
+        arrayOfOperands[2] = Operand.parseOperand("5.6x");
+        arrayOfOperands[3] = Operand.parseOperand("6x^3");
+        arrayOfOperands[4] = Operand.parseOperand("-7.9x^3");
+        arrayOfOperands[5] = Operand.parseOperand("2x^2");
+        arrayOfOperands[6] = Operand.parseOperand("4.2x^4");
+        arrayOfOperands[7] = Operand.parseOperand("0x");
+        arrayOfOperands[8] = Operand.parseOperand("-9x^5");
+        arrayOfOperands[9] = Operand.parseOperand("0.9x^5");
         
         Expression expression = new Expression();
         for (int i = 0; i < 10; i++) {
@@ -32,14 +36,21 @@ class Test {
         }
         
         System.out.print("\n" + "Original input: ");
-        expression.printExpression();        
-
+        expression.printExpression();
+    
         Expression standardizedExpression = new Expression(Expression.standardizeExpression(expression));
         System.out.print("\n\n" + "After standardized: ");
         standardizedExpression.printExpression();
-        
-        Expression simplifiedExpression = new Expression(Expression.standardizeExpression(expression));
+
+        Expression simplifiedExpression = new Expression(Expression.simplifyExpression(expression));
         System.out.print("\n\n" + "After simplified: ");
         simplifiedExpression.printExpression();
+
+        // Operand operand;
+        // for (int i = 1; i <= 10; i++) {
+        //     operand = generateRandomOperand();
+        //     operand.printOperand();
+        //     System.out.println();
+        // }
     }
 }
